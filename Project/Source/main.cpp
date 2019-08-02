a566bf22e92240d37016669f6e989997a76d2b30
Starly Solon <starlysolon@gmail.com>
2019-08-02 13:09:41 -0400

    Fixed loading empty scenes on platforms other than Windows

--- Project/Source/main.cpp
+++ Project/Source/main.cpp
@@ -29,7 +29,6 @@ int main(int argc, char*argv[])
 		// TODO - You can alternate between different scenes for testing different things
 		// Static Scene contains no animation
 		// Animated Scene does
-
 #if defined(PLATFORM_OSX)
 //		world.LoadScene("Scenes/AnimatedSceneWithParticles.scene");
 //		world.LoadScene("Scenes/AnimatedScene.scene");
